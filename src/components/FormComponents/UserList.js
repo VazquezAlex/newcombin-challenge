06fb7048b61305f61c7b9f313cc4d0f97ca2e1a3
Alejandro Vazquez del Mercado Gutiérrez <alejandro_vazquez98@hotmail.com>
2022-06-04 11:28:41 -0500

    Base user table layout

--- src/components/FormComponents/UserList.js
+++ src/components/FormComponents/UserList.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
 export const UserList = () => {
-  return (
-    <div>UserList</div>
-  )
+	return (
+		<div className = ' userlist_container '>
+			<table className = ' userlist_table pad-x-1 '>
+				<tr className = ' userlist_table-header '>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>Address</th>
+					<th>SSN</th>
+				</tr>
+				<tr>
+					<td>Alfreds Futterkiste</td>
+					<td>Maria Anders</td>
+					<td>Germany</td>
+					<td>3323-23-23</td>
+				</tr>
+			</table>
+
+		</div>
+	)
 }
