1ea54019c94f9579aab6a42658d7a52e0ab4732e
Alejandro Vazquez del Mercado Gutiérrez <alejandro_vazquez98@hotmail.com>
2022-06-04 11:20:10 -0500

    Base form design

--- src/components/FormComponents/Form.js
+++ src/components/FormComponents/Form.js
@@ -2,6 +2,44 @@ import React from 'react'
 
 export const Form = () => {
     return (
-        <div>Form</div>
+        <div className = ' pad-1 '>
+
+            <form
+                className = ' pad-x-1 '
+            >
+
+                <input 
+                    className = ' ui_input margin-bot-1 '
+                    placeholder = 'First Name'
+                />
+                <input 
+                    className = ' ui_input margin-bot-1 '
+                    placeholder = 'Last Name'
+                />
+                <input 
+                    className = ' ui_input margin-bot-1 '
+                    placeholder = 'Address Name'
+                />
+                <input 
+                    className = ' ui_input margin-bot-1 '
+                    placeholder = 'SSN'
+                />
+
+                <div className = ' form_cta-container pad-top-1 '>
+                    <button
+                        className = ' buttons_secondary margin-end-1 '
+                    >
+                        Reset
+                    </button>
+                    <button
+                        className = ' buttons_main '
+                    >
+                        Enviar
+                    </button>
+                </div>
+
+            </form>
+
+        </div>
     )
 }
